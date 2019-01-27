5f3e0d42fe7c56862b0d5fd79563064193985d10
fabio <fjpdbruijn@hotmail.com>
2019-01-27 23:48:11 +0100

    news

---
+++ build1/precache-manifest.1cdda3ea9f6e3abfae86d06da3c90210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d4ed92f38ef78a1ca17a",
+    "url": "/static/js/main.d4ed92f3.chunk.js"
+  },
+  {
+    "revision": "ad17dcfc959246abf65a",
+    "url": "/static/js/1.ad17dcfc.chunk.js"
+  },
+  {
+    "revision": "d4ed92f38ef78a1ca17a",
+    "url": "/static/css/main.052fd702.chunk.css"
+  },
+  {
+    "revision": "cec4b6600ea21b3b281e7340f9ae1f97",
+    "url": "/index.html"
+  }
+];
